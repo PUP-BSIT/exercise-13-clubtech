fb658ecad2e4e1c75918a25128284fddc500eb85
Lazaro-Franco <francoalfonso411@gmail.com>
2024-12-11 12:08:22 +0800

    "add changes to the code"

--- Lazaro.py
+++ Lazaro.py
@@ -6,18 +6,18 @@ print("4-Exit")
 user_input =  int(input("Enter your choice: "))
 
 if user_input == 1:
-    print("Age: 69")
+    print("Age: 19")
     print("Birthday: April 11, 2005")
-    print("Hobby: Watching anime, reading manga, Drawing")
+    print("Hobby: Watching anime, reading manga, drawing")
     print("Favorite Food: Okonomiyaki")
     print("Favorite car: Citroen 2cv, Fiat nuova 500")
-    print("Favorite Anime: Lupin IV, Assassination Classroom")
+    print("Favorite Anime: Lupin III, Assassination Classroom")
     
 elif user_input == 2:
    print("==== Goals ====") 
    print("To become an artist or game developer")
    print("Create my own animated series")
-   print("To become a Great animator")
+   print("To become a great animator")
     
 elif user_input == 3:
     #teammates comments
