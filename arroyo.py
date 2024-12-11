a84c38801c171e147ac08e5e4aac3ce2f061c6c4
John Matthew Saraga Arroyo <johnmatthewarroyo2@gmail.com>
2024-12-11 14:52:04 +0800

    indent each module to become function

--- arroyo.py
+++ arroyo.py
@@ -1,30 +1,31 @@
-user_input = 0
-while user_input != 4:
-    print("---------------------------------")
-    print("Greetings, John Matthew Arroyo")
-    print("1. Basic Info")
-    print("2. Goals")
-    print("3. Comments from teammates")
-    print("4. Exit")
-    print("---------------------------------")
-    user_input = int(input("Choose your option: "))
+def arroyo_menu():
+    user_input = 0
+    while user_input != 4:
+        print("---------------------------------")
+        print("Greetings, John Matthew Arroyo")
+        print("1. Basic Info")
+        print("2. Goals")
+        print("3. Comments from teammates")
+        print("4. Exit")
+        print("---------------------------------")
+        user_input = int(input("Choose your option: "))
     
-    if user_input == 1:
-        print("Age: 19")
-        print("Birthday: January 1, 2005")
-        print("Location: Pasay City")
-        print("Hobbies: Cycling, reading, playing pc games")
-        print("Favorite Food: Chicken Curry and bonding with pizza")
-        print("Favorite Anime: One Piece")
+        if user_input == 1:
+            print("Age: 19")
+            print("Birthday: January 1, 2005")
+            print("Location: Pasay City")
+            print("Hobbies: Cycling, reading, playing pc games")
+            print("Favorite Food: Chicken Curry and bonding with pizza")
+            print("Favorite Anime: One Piece")
         
-    elif user_input == 2:
-        print("My goals are: ")
-        print("-graduate college and become a good programmer by developing my IT skills")
-        print("-develop myself physically and spiritually through exercise and bible reading")
+        elif user_input == 2:
+            print("My goals are: ")
+            print("-graduate college and become a good programmer by developing my IT skills")
+            print("-develop myself physically and spiritually through exercise and bible reading")
         
-    elif user_input == 3:
-        print("This code has been reviewed by Franco Alfonso Lazaro")
-        print("Greetings from John Carlo I. Nolluda")
-        
+        elif user_input == 3:
+            print("This code has been reviewed by Franco Alfonso Lazaro")
+            print("Greetings from John Carlo I. Nolluda")
+
 
 
--- nolluda.py
+++ nolluda.py
@@ -1,30 +1,31 @@
-user_input = 0
-while user_input != 4:
-    print("==============================")
-    print("Greetings! John Carlo I. Nolluda")
-    print("1. Basic info")
-    print("2. Goals")
-    print("3. Comments from teammates")
-    print("4. Exit")
-    print("==============================")
-    user_input = int(input("Enter your option: "))
+def nolluda_menu():
+    user_input = 0
+    while user_input != 4:
+        print("==============================")
+        print("Greetings! John Carlo I. Nolluda")
+        print("1. Basic info")
+        print("2. Goals")
+        print("3. Comments from teammates")
+        print("4. Exit")
+        print("==============================")
+        user_input = int(input("Enter your option: "))
 
-    if user_input == 1:
-        print("===============================================================")
-        print("Age: 20")
-        print("Birthday: September 18, 2004")
-        print("Location: Guiho St. Creekland Hagonoy, Taguig City")
-        print("Hobbies: Ml, Cycling, Playing lol")
-        print("Favorite foods: Bicol express, sinigang na baboy, adobong baboy ")
-        print("================================================================")
+        if user_input == 1:
+            print("===============================================================")
+            print("Age: 20")
+            print("Birthday: September 18, 2004")
+            print("Location: Guiho St. Creekland Hagonoy, Taguig City")
+            print("Hobbies: Ml, Cycling, Playing lol")
+            print("Favorite foods: Bicol express, sinigang na baboy, adobong baboy ")
+            print("================================================================")
 
-    elif user_input == 2:
-        print("=================")
-        print("My goals are: ")
-        print("finish college")
-        print("buy bike/motor")
-        print("=================")
+        elif user_input == 2:
+            print("=================")
+            print("My goals are: ")
+            print("finish college")
+            print("buy bike/motor")
+            print("=================")
     
-    elif user_input == 3:
-        print("This code has been reviewed by Franco Alfonso Lazaro ")
-        print("This code has been reviewed by John Matthew Arroyo") 
+        elif user_input == 3:
+            print("This code has been reviewed by Franco Alfonso Lazaro ")
+            print("This code has been reviewed by John Matthew Arroyo") 
