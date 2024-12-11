02cff1b65471b413ce02137ba27e1deac43f20ec
John Matthew Saraga Arroyo <johnmatthewarroyo2@gmail.com>
2024-12-11 11:16:24 +0800

    add arroyo.py code from branch-1

---
+++ arroyo.py
@@ -0,0 +1,29 @@
+user_input = 0
+while user_input != 4:
+    print("---------------------------------")
+    print("Greetings, John Matthew Arroyo")
+    print("1. Basic Info")
+    print("2. Goals")
+    print("3. Comments from teammates")
+    print("4. Exit")
+    print("---------------------------------")
+    user_input = int(input("Choose your option: "))
+    
+    if user_input == 1:
+        print("Age: 199")
+        print("Birthday: January 1, 2005")
+        print("Location: Pasay City")
+        print("Hobbies: Cycling, reading, playing pc games")
+        print("Favorite Food: Chicken Curry and bonding with pizza")
+        print("Favorite Anime: One Piece")
+        
+    elif user_input == 2:
+        print("My goals are: ")
+        print("-graduate college and become a good programmer by developing my IT skills")
+        print("-develop myself physically and spiritually through exercise and bible reading")
+        
+    elif user_input == 3:
+        pass #Teammate comments will go here
+        
+
+
