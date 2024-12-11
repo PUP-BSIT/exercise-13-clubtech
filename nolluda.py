70fa31754f7edd6d4148d6e168dbc43e82cf3625
nolluda-johncarlo <johncarlonolluda@gmail.com>
2024-12-11 13:55:40 +0800

    add code  nolluda.py from feature-1
    * add code  nolluda.py from feature-1
    
    * add changes
    
    * add changes to "Location" in line 16

---
+++ nolluda.py
@@ -0,0 +1,29 @@
+user_input = 0
+while user_input != 4:
+    print("==============================")
+    print("Greetings! John Carlo I. Nolluda")
+    print("1. Basic info")
+    print("2. Goals")
+    print("3. Comments from teammates")
+    print("4. Exit")
+    print("==============================")
+    user_input = int(input("Enter your option: "))
+
+    if user_input == 1:
+        print("===============================================================")
+        print("Age: 20")
+        print("Birthday: September 18, 2004")
+        print("Location: Guiho St. Creekland Hagonoy, Taguig City")
+        print("Hobbies: Ml, Cycling, Playing lol")
+        print("Favorite foods: Bicol express, sinigang na baboy, adobong baboy ")
+        print("================================================================")
+
+    elif user_input == 2:
+        print("=================")
+        print("My goals are: ")
+        print("finish college")
+        print("buy bike/motor")
+        print("=================")
+    
+    elif user_input == 3:
+        pass 
