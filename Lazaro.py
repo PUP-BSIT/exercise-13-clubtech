fe82251d29e3404f54a222ae779a5d27b02f20c2
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2024-12-11 14:58:26 +0800

    indent each module to become function

--- Lazaro.py
+++ Lazaro.py
@@ -1,28 +1,29 @@
-print("Greetings: Franco Alfonso C. Lazaro")
-print("1-Basic Info")
-print("2-Goals ")
-print("3-Comments from teammates")
-print("4-Exit")
-user_input =  int(input("Enter your choice: "))
+def lazaro_menu():
+    print("Greetings: Franco Alfonso C. Lazaro")
+    print("1-Basic Info")
+    print("2-Goals ")
+    print("3-Comments from teammates")
+    print("4-Exit")
+    user_input =  int(input("Enter your choice: "))
 
-if user_input == 1:
-    print("Age: 19")
-    print("Birthday: April 11, 2005")
-    print("Hobby: Watching anime, reading manga, drawing")
-    print("Favorite Food: Okonomiyaki")
-    print("Favorite car: Citroen 2cv, Fiat nuova 500")
-    print("Favorite Anime: Lupin III, Assassination Classroom")
+    if user_input == 1:
+        print("Age: 19")
+        print("Birthday: April 11, 2005")
+        print("Hobby: Watching anime, reading manga, drawing")
+        print("Favorite Food: Okonomiyaki")
+        print("Favorite car: Citroen 2cv, Fiat nuova 500")
+        print("Favorite Anime: Lupin III, Assassination Classroom")
     
-elif user_input == 2:
-   print("==== Goals ====") 
-   print("To become an artist or game developer")
-   print("Create my own animated series")
-   print("To become a great animator")
+    elif user_input == 2:
+        print("==== Goals ====") 
+        print("To become an artist or game developer")
+        print("Create my own animated series")
+        print("To become a great animator")
     
-elif user_input == 3:
-    #teammates comments
-    print("This code has been reviewed by John Carlo Nolluda")
-    print("This code has been reviewed by John Matthew Arroyo")
+    elif user_input == 3:
+        #teammates comments
+        print("This code has been reviewed by John Carlo Nolluda")
+        print("This code has been reviewed by John Matthew Arroyo")
 
-else:
-    pass 
+    else:
+        pass 
